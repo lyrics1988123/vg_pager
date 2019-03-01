a7aedceaa55e154a870fe0998642cf244c82aa9c
lyrics1988123 <35185804+lyrics1988123@users.noreply.github.com>
2019-03-01 04:21:21 +0100

    Message can be sent now too

--- vg_pager/client.lua
+++ vg_pager/client.lua
@@ -1,17 +1,9 @@
 
 
-RegisterCommand('pager', function(source, args, raw)
-	if args[1] ~= nil then
-		local targetPlayer = tonumber(args[1])
-		TriggerServerEvent('vg_pager:Pager', targetPlayer)
-	end	
-end, false)
 
-
-RegisterNetEvent('vg_pager:Pager')
-AddEventHandler('vg_pager:Pager', function(targetPlayer)
-	local targetPlayerNetId = NetworkGetNetworkIdFromEntity(targetPlayer)
-	--or: local targetPlayerNetId = targetPlayer ----> please test it
-	TriggerServerEvent("InteractSound_SV:PlayOnOne", targetPlayerNetId, "soundfile", 0.6) --0.6 is volume
+RegisterNetEvent('vg_pager:receiveNotification')
+AddEventHandler('vg_pager:receiveNotification', function(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, true)
 end)
-
--- vg_pager/server.lua
+++ vg_pager/server.lua
@@ -1,4 +1,16 @@
-RegisterServerEvent("vg_pager:Pager")
-AddEventHandler("vg_pager:Pager", function(targetPlayer)
-	TriggerClientEvent("vg_pager:Pager", targetPlayer)
-end)
+
+
+
+RegisterCommand('pager', function(source, args, raw)
+if args[1] ~= nil then
+		local targetPlayerNetId = tonumber(args[1])
+		--or: local targetPlayerNetId = NetworkGetNetworkIdFromEntity(tonumber(args[1])) ----> please test it
+		TriggerEvent("InteractSound_SV:PlayOnOne", targetPlayerNetId, "soundfile", 0.6) --0.6 is volume
+		if args[2] ~= nil then
+			local text = args[2]
+			TriggerClientEvent("vg_pager:ShowNotification", targetPlayer, text)
+		end
+	end	
+end, false)
+
+
