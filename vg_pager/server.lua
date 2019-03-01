48d45c3a7ba1a82aceb4e2e819e9579e9892e01f
lyrics1988123 <35185804+lyrics1988123@users.noreply.github.com>
2019-03-01 05:36:23 +0100

    Add files via upload

--- vg_pager/server.lua
+++ vg_pager/server.lua
@@ -1,16 +1,19 @@
-
-
-
 RegisterCommand('pager', function(source, args, raw)
 if args[1] ~= nil then
 		local targetPlayerNetId = tonumber(args[1])
 		--or: local targetPlayerNetId = NetworkGetNetworkIdFromEntity(tonumber(args[1])) ----> please test it
 		TriggerEvent("InteractSound_SV:PlayOnOne", targetPlayerNetId, "soundfile", 0.6) --0.6 is volume
 		if args[2] ~= nil then
-			local text = args[2]
+            local text = ""
+            for i=2, #args, 1 do
+                if (i == 2) then
+                    text = args[i]
+                else
+                    text = text .. " " .. args[i]
+                end
+            end
+        
 			TriggerClientEvent("vg_pager:ShowNotification", targetPlayerNetId, text)
 		end
 	end	
 end, false)
-
-
